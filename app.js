908ff400e164c68bb31386c8e56451664851fdd3
nirglus <nirgluskin@gmail.com>
2023-11-22 01:21:47 +0200

    add body parser

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-
+app.use(bodyParser.urlencoded({ extended: true}))
 app.get("/", (req, res) =>{
     res.render("index.ejs");
 })
