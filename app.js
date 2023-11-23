47c3218a1108cdbf266162c133f3bbf58912ab60
nirglus <nirgluskin@gmail.com>
2023-11-23 18:54:32 +0200

    add API_URL

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://v2.jokeapi.dev";
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.get("/", (req, res) =>{
     res.render("index.ejs");
-})
+});
 
 app.listen(port, () =>{
     console.log(`Server is up and running on ${port}`);
-})
+});
