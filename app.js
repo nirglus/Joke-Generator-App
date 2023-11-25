86083b5b865551112d0592ff01606b2d48e9a17a
nirglus <nirgluskin@gmail.com>
2023-11-25 11:38:01 +0200

    add type for post

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const API_URL = "https://v2.jokeapi.dev";
+const API_URL = "https://v2.jokeapi.dev/joke/Any";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true}))
 app.get("/", (req, res) =>{
     res.render("index.ejs");
 });
+
+app.post("/", async (req, res) =>{
+    console.log(req.body);
+    const singleChecked = req.body.type === "single";
+    const twopartChecked = req.body.type === "twopart";
+    let type = "";
+    if (singleChecked && !twopartChecked) {
+      type = "type=single";
+    } else if (!singleChecked && twopartChecked) {
+      type = "type=twopart";
+    }
+    try {
+        const result = await axios.get(API_URL + "")
+    } catch (error) {
+        
+    }
+})
 
 app.listen(port, () =>{
     console.log(`Server is up and running on ${port}`);
